700af8cb9944f8dc6bfe01cdf32c144b33bc88ff
Don Mennerich <don@orcstorm.net>
2020-06-19 16:25:42 -0400

    Adding gopher

--- main.go
+++ main.go
@@ -6,13 +6,27 @@ import (
 	"log"
 )
 
+var img *ebiten.Image
+
 type Game struct{}
+
+func init() {
+	var err error
+	img, _, err = ebitenutil.NewImageFromFile("assets/gopher.png", ebiten.FilterDefault)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func (g *Game) Update(screen *ebiten.Image) error {
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Translate(1, 1)
+	op.GeoM.Scale(.5, .5)
+	screen.DrawImage(img, op)
 	ebitenutil.DebugPrint(screen, "Matze!")
 }
 
